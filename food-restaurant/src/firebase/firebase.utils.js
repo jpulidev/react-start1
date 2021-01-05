88c5e13c1b0737ea47e096ea77590fa1322b2fc5
Jose Pulido <josecpulidoo@gmail.com>
2021-01-05 17:04:44 -0400

    Create Database async await for auth users

--- food-restaurant/src/firebase/firebase.utils.js
+++ food-restaurant/src/firebase/firebase.utils.js
@@ -11,6 +11,29 @@ const config = {
     appId: "1:248593433739:web:4b40d22ecbe8e05e88c08e",
     measurementId: "G-6CC3JGHGTY"
   };
+ export const createUserProfileDocument = async (userAuth, additionalData) => {
+   if (!userAuth) return;
+   const userRef = firestore.doc(`users/${userAuth.uid}`)
+   const snapShot = await userRef.get();
+   if(!snapShot.exists) {
+       const {displayName, email} = userAuth;
+       const createdAt = new Date();
+       try {
+           await userRef.set({
+               displayName,
+               email,
+               createdAt,
+               ...additionalData
+           })
+
+       } catch (error){
+           console.log('error creating user', error.message);
+
+       }
+   }
+   return userRef;
+ };
+
 
   firebase.initializeApp(config);
 
