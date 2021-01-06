4de36fbaab7995510f3f07ae0ea22d2217e7b8e2
Jose Pulido <josecpulidoo@gmail.com>
2021-01-06 16:45:15 -0400

    Finishing sign in handle submit method

--- food-restaurant/src/components/sign-in/sign-in.component.jsx
+++ food-restaurant/src/components/sign-in/sign-in.component.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import FormInput from '../form-input/form-input.component';
 import './sign-in.styles.scss';
-import { signInWithGoogle } from '../../firebase/firebase.utils';
+import { auth, signInWithGoogle } from '../../firebase/firebase.utils';
 import CustomButton from '../custom-button/custom-button.component';
 class SignIn extends React.Component{
     constructor(props){
@@ -11,9 +11,18 @@ class SignIn extends React.Component{
             password:''
         }
     }
-handleSubmit = event => {
+handleSubmit = async event => {
     event.preventDefault();
-    this.setState({email:'', password:''})
+
+    const {email, password} = this.state;
+
+    try {
+        await auth.signInWithEmailAndPassword(email, password);
+        this.setState({email:'', password:''})
+    } catch (error) {
+        console.log(error);
+    }
+    
 }
 
 handleChange = event => {
@@ -39,7 +48,7 @@ handleChange = event => {
             <FormInput 
             name='password' 
             type='password' 
-            value={this.state.email} 
+            value={this.state.password} 
             handleChange={this.handleChange}
             label='password'
             required />
