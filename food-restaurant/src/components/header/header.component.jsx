826ded96f6f2a6c76a9874c37d8448646fbdc332
Jose Pulido <josecpulidoo@gmail.com>
2021-01-08 09:53:49 -0400

    Create Cart-dropdown and add to the header component with the style component

--- food-restaurant/src/components/header/header.component.jsx
+++ food-restaurant/src/components/header/header.component.jsx
@@ -5,6 +5,8 @@ import {ReactComponent as Logo} from '../../assets/crown.svg'
 import {auth} from '../../firebase/firebase.utils';
 import {connect} from 'react-redux';
 import CartIcon from '../cart-icon/cart-icon.component';
+import CartDropdown from '../cart-dropdown/cart-dropdown.component';
+
 
 
 const Header = ({ currentUser }) => (
@@ -31,6 +33,7 @@ const Header = ({ currentUser }) => (
             )}
             <CartIcon/>
    </div>
+   <CartDropdown/>
    </div>
 );
 const mapStateToProps = state => ({
