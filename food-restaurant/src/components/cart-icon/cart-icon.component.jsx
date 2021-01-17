f77f312e144f6ad10be2505d5a65ba1da167223d
Jose Pulido <josecpulidoo@gmail.com>
2021-01-17 19:45:17 -0400

    Create Cart Selector component Function Memoize

--- food-restaurant/src/components/cart-icon/cart-icon.component.jsx
+++ food-restaurant/src/components/cart-icon/cart-icon.component.jsx
@@ -4,6 +4,7 @@ import {ReactComponent as ShoppingIcon} from '../../assets/shopping-bag.svg';
 import {connect} from 'react-redux';
 
 import {toggleCartHidden} from '../../redux/cart/cart.actions';
+import {selectCartItemsCount} from '../../redux/cart/cart.selectors';
 
 
 const CartIcon = ({toggleCartHidden, itemCount}) => (
@@ -17,8 +18,8 @@ const mapDispatchToProps = dispatch => ({
     toggleCartHidden: () => dispatch(toggleCartHidden())
 });
 
-const mapStateToProps = ({cart: {cartItems}} ) => ({
-    itemCount: cartItems.reduce((accumaltedQuantity, cartItem) => accumaltedQuantity + cartItem.quantity, 0)
+const mapStateToProps = (state) => ({
+    itemCount: selectCartItemsCount(state)
 });
 
 export default connect(
